0295567a93a41212be64357ad0df64882d3fe66a
Tenzin_VR <tenzinvr@gmail.com>
2018-10-01 15:15:13 +0200

    test

--- calculator/calculator/MainWindow.xaml.cs
+++ calculator/calculator/MainWindow.xaml.cs
@@ -21,8 +21,10 @@ namespace calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        double pi = 3.1415926;
-
+        decimal pi = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914m;
+        decimal rt2 = 1.41421356237309504880168872420969807856967187537694807317667973799073247846210703885038753432764157273501384623091229702492483605585073721264412149709993583141322266592750559275579995050115278206057147010955997160597027453459686201472851m;
+        decimal rt3 = 1.73205080756887729352744634150587236694280525381038062805580697945193301690880003708114618675724857567562614141540670302996994509499895247881165551209437364852809323190230558206797482010108467492326501531234326690332288665067225466892183m;
+        bool specCase = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,14 +36,14 @@ namespace calculator
         /// <param name="baseNum"> Value of base </param>
         /// <param name="exp"> Exponent </param>
         /// <returns> Base raised to exponent </returns>
-        public double exponent(double baseNum, double exp)
+        public double exponent(decimal baseNum, decimal exp)
         {
             bool pos = (exp >= 0) ? true : false;
             exp = (exp >= 0) ? exp : -exp;
-            double res = 1;
-            for (int b = 0; b < exp; b++) { res = res*baseNum; }
-            if (pos) return res;
-            return 1 / res;
+            decimal dcRes = 1;
+            for (int b = 0; b < exp; b++) { dcRes = dcRes*baseNum; }
+            if (pos) return Convert.ToDouble(dcRes);
+            return 1 / Convert.ToDouble(dcRes);
         }
 
         /// <summary>
@@ -57,6 +59,20 @@ namespace calculator
                 factRes = factRes * x;
             }
             return factRes;
+        }
+
+        public double logarithms(double b, double n)
+        {
+            double logRes = 0;
+            int x, comp;
+            while (true)
+            {
+                x = 
+                comp = n - exponent(b, x)
+                if (comp > 0) { }
+            }
+
+            return logRes;
         }
 
         /*
@@ -80,10 +96,13 @@ namespace calculator
         /// <param name="funct"> Trig function </param>
         /// <param name="theta"> Angle </param>
         /// <returns> Value of function at given angle </returns>
-        public double trigonometry(string funct, double theta)
+        public double trigonometry(string funct, decimal theta)
         {
-            theta = theta * (pi / 180);
-            double trigRes = 2;
+            decimal[] trigSpecCase = { 0, pi/2, pi, (3/4)*pi, 2*pi };
+            //theta = theta * (pi / 180);
+            double trigRes = 0;
+            if (trigSpecCase.Contains(theta)) { specCase = true; }
+
             switch (funct)
             {
                 case ("sin"):
@@ -104,27 +123,34 @@ namespace calculator
                     return Sec(theta);
                 case ("cot"):
                     return Cot(theta);
-                
             }
             return trigRes;
         }
 
-        public double Sin(double theta)
+        public double Sin(decimal theta)
         {
+            if (specCase == true)
+            {
+                decimal[] sinSpecCase = { 0m, 0.5m, rt2 / 2, rt3 / 2, 1 };
+                int i = Convert.ToInt32((theta / pi) * 4);
+                return Convert.ToDouble(sinSpecCase[i]);
+            }
             int len = 50;
             int n;
-            double sinRes = theta;
+            decimal[] lsSinRes = new decimal[len];
+            double dbSinRes = 0;
             int[] exp = new int[len];
             for (int num = 0; num < len; num++) { exp[num] = 2 * num + 1; };
             foreach (int x in exp)
             {
-                n = x / 2;
-                sinRes += exponent(-1, n) * exponent(theta, x) / factorial(x);
+                n = x/2;
+                lsSinRes[n] = Convert.ToDecimal(exponent(-1, n) * exponent(theta, x) / factorial(x));
             }
-            return sinRes;
+            dbSinRes += Convert.ToDouble(lsSinRes.Sum());
+            return dbSinRes;
         }
-        public double Arcsin(double theta) { return (Sin(theta)); }
-        public double Cosec(double theta)
+        public double Arcsin(decimal theta) { return (Sin(theta)); }
+        public double Cosec(decimal theta)
         {
             try
             {
@@ -134,11 +160,11 @@ namespace calculator
             return -1;
         }
 
-        public double Cos(double theta)
+        public double Cos(decimal theta)
         {
             int len = 50;
             int n;
-            double cosRes = theta;
+            double cosRes = 0;
             int[] exp = new int[len];
             for (int num = 0; num < len; num++) { exp[num] = 2 * num; };
             foreach (int x in exp)
@@ -148,8 +174,8 @@ namespace calculator
             }
             return cosRes;
         }
-        public double Arccos(double theta) { return (Cos(theta)); }
-        public double Sec(double theta)
+        public double Arccos(decimal theta) { return (Cos(theta)); }
+        public double Sec(decimal theta)
         {
             try
             {
@@ -159,22 +185,33 @@ namespace calculator
             return -1;
         }
 
-        public double Tan(double theta)
+        public double Tan(decimal theta)
         {
-            double tanRes = 2;
-            return tanRes;
+            try
+            {
+                return Sin(theta) / Cos(theta);
+            }
+            catch (DivideByZeroException e) { MessageBox.Show(e.ToString(), "Maths Error"); }
+            return -1;
         }
-        public double Arctan(double theta) { return (Tan(theta)); }
-        public double Cot(double theta) { return 1 / Tan(theta); }
+        public double Arctan(decimal theta) { return (Tan(theta)); }
+        public double Cot(decimal theta) { return 1 / Tan(theta); }
 
         private void sinBtn_Click(object sender, RoutedEventArgs e)
         {
             // Sin tests
             Tester.TestEq(Sin(0), 0);
-            Tester.TestEq(Sin(3.1415926/2), 1);
+            Tester.TestEq(Sin(pi/2), 1.0);
             // Cos tests
             Tester.TestEq(Cos(0), 1);
-            Tester.TestEq(Cos(3.1415926/2), 0);
+            Tester.TestEq(Cos(pi/2), 0);
+            // Tan Tests
+            Tester.TestEq(Tan(0), 0);
+            Tester.TestEq(Cos(pi/2), -1);
+            //Factorial Tests
+            Tester.TestEq(factorial(0), 1);
+            Tester.TestEq(factorial(1), 1);
+            Tester.TestEq(factorial(4), 24);
         }
     }
 
